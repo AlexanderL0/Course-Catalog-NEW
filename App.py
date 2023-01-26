088b31c22ab9ec067c2b422b7b1a153cb365c408
MichaelBoelens37 <mboelens37@gmail.com>
2023-01-26 08:58:54 -0600

    data

--- App.py
+++ App.py
@@ -1,33 +1,20 @@
+import sqlite3
 from flask import Flask, render_template
-##from flask_sqlalchemy import SQLAlchemy
-
-
-
 
 app = Flask (__name__)
-##app.secret_key = "jChung"
 
-##app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud'
-##app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def get_db_connection():
+    conn = sqlite3.connect('CourseCatalog.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-##db = SQLAlchemy(app)
-
-
-##class Data (db.Model):
-##    id = db.Column(db.Integer, primary_key = True)
-  ##  name =db.Column(db.String(100))
-    ##teacher =db.Column(db.String(100))
-    ##subject =db.Column(db.String(100))
-
-    ##def __init__(self, name, teacher, subject):
-      ##  self.name = name
-        ##self.teacher = teacher
-        ##self.subject = subject
-        
 
 @app.route('/')
-def Index():
-    return render_template("index.html")
+def index():
+    conn = get_db_connection()
+    lstCourse = conn.execute('''select * from Catalog''').fetchall()
+    conn.close()
+    return render_template("index.html", lstCourse=lstCourse)
 
 @app.route('/psych')
 def Psych():
@@ -37,9 +24,9 @@ def Psych():
 def Comp():
     return render_template("comp.html")
 
-@app.route('/econ')
+@app.route('/ClassTemplate')
 def Econ():
-    return render_template("econ.html")
+    return render_template("ClassTemplate.html")
 
 @app.route('/stats')
 def Stats():
--- Db.py
+++ Db.py
@@ -4,9 +4,9 @@ import sqlite3
 print(sqlite3.version)
 print(sqlite3.sqlite_version)
 
-db = sqlite3.connect('CourseC.db')
+db = sqlite3.connect('CourseCatalog.db')
 cursor = db.cursor()
 
-lstCourse = cursor.execute('''select  credits from Course_Info''').fetchall()
+lstCourse = cursor.execute('''select name, subject from Catalog''').fetchall()
 db.commit()
 print(lstCourse)
