f0916080dc96db057b7617cac42f7a0167032a1b
MichaelBoelens37 <mboelens37@gmail.com>
2023-02-01 10:09:03 -0600

    Feb1

--- App.py
+++ App.py
@@ -16,49 +16,13 @@ def index():
     conn.close()
     return render_template("index.html", lstCourse=lstCourse)
 
-@app.route('/psych')
-def Psych():
-    return render_template("psych.html")
-  
-@app.route('/comp')
-def Comp():
-    return render_template("comp.html")
-
-@app.route('/ClassTemplate')
-def Econ():
+@app.route('/class')
+def Template():
     return render_template("ClassTemplate.html")
 
-@app.route('/stats')
-def Stats():
-    return render_template("stats.html")
-
-@app.route('/multi')
-def Multi():
-    return render_template("multi.html")
-
-@app.route('/bc')
-def Bc():
-    return render_template("bc.html")
-
-@app.route('/APUSH')
-def Apush():
-    return render_template("APUSH.html")
-
-@app.route('/chinese4')
-def Chinese4():
-    return render_template("chinese4.html")
-
-@app.route('/lang')
-def Lang():
-    return render_template("lang.html")
-
-@app.route('/physicsC')
-def PhysicsC():
-    return render_template("physicsC.html")
-
-
-
-
+@app.route('/honors')
+def Honor():
+    return render_template("honors.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
